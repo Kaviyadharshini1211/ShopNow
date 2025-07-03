2f0c7dfa8e4d95294062449d0b5ab9e46fda078e
Kaviyadharshini1211 <kaviyadharshinim3@gmail.com>
2025-07-03 20:43:43 +0530

    Update CategoryPage.js

--- frontend/src/pages/CategoryPage.js
+++ frontend/src/pages/CategoryPage.js
@@ -11,7 +11,7 @@ const CategoryPage = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await axios.get('http://localhost:5000/api/products');
+        const res = await axios.get('https://shoppica.onrender.com/api/products');
         const allProducts = res.data;
 
         const filtered = allProducts.filter(
