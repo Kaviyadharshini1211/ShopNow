a30ebfae02fdb09ec8bcae1ea2b003c2e513b56a
Kaviyadharshini1211 <kaviyadharshinim3@gmail.com>
2025-07-03 20:52:01 +0530

    Update productServices.js

--- frontend/src/services/productServices.js
+++ frontend/src/services/productServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/products";
+const API_URL = "https://shoppica.onrender.com/api/products";
 
 // Fetch All Products
 export const getProducts = async () => {
